330b50362db77e89508ea64380d4a499bc967692
Amir <golizadeh74@gmail.com>
2025-04-03 15:02:30 +0100

    Add New Book page complete

---
+++ src/Form/BookFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('author', TextType::class)
+            ->add('numberOfPages', IntegerType::class)
+            ->add('summary', TextareaType::class)
+            ->add('genre', TextType::class)
+            ->add('coverImagePath', FileType::class, [
+                'label' => 'Cover Image',
+                'required' => false,
+                'mapped' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
